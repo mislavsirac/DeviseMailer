60263a631012e3457e514506d771fdce638b97c0
Ubuntu <ubuntu@ip-172-31-8-171.eu-west-2.compute.internal>
2022-05-26 23:46:02 +0000

    added user article associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_200441) do
+ActiveRecord::Schema.define(version: 2022_05_26_231254) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
